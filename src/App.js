71ff432ef67749bb3d2cca7e02225a8241d36f1d
kedarr12 <kedarmulay1@gmail.com>
2022-06-20 21:04:03 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// components
+import Navbar from "./components/navbar";
+
+function App() {
+    return (
+        <div className="App">
+            <Navbar />
+            <main>
+                <h1>Heyy! I am Kedarr.</h1>
+                <p>Website in development.</p>
+                <p>Coffee ❤️ </p>
+            </main>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+// react
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+
+// App
+import App from './App';
+
+// styles
+import './index.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  	<React.StrictMode>
+    	<App />
+  	</React.StrictMode>
+);
+
+reportWebVitals();
